4fe3fa3419a9ae80e1c3505133596d19ed9eccdf
FerchoAlone <dfernandezd@unicauca.edu.co>
2024-11-20 12:21:40 -0500

    Add Actuator

--- user_management_ms/src/main/java/com/activate/ActivateMSV1/user_management_ms/UserManagementMsApplication.java
+++ user_management_ms/src/main/java/com/activate/ActivateMSV1/user_management_ms/UserManagementMsApplication.java
@@ -1,14 +1,19 @@
 package com.activate.ActivateMSV1.user_management_ms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserManagementMsApplication {
 
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(UserManagementMsApplication.class, args);
+		Logger logger = LoggerFactory.getLogger(UserManagementMsApplication.class);
+		logger.info("Corriendo con actuator");
 	}
 
 }
